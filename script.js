e2f6cfbcd325520a0701da1c5a8e12667926788b
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-12-01 21:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 class Person {
-	constructor(name , age) {
-		this.name = name;
-		this.age = age;
-	}
-	get name (){
-		return this.name;
-	}
-	get age (){
-		return this.age;
-	}
-	set age(value){
-		this.age = value;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(value) {
+    this._age = value;
+  }
 }
+
 class Student extends Person {
-	study() {
-		console.log(`${this.name} is studying`)
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
+
 class Teacher extends Person {
   teach() {
     console.log(`${this.name} is teaching`);
   }
 }
+
+// FIX (required for Cypress)
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
