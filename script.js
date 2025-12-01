3618b494d083aa4047ba089e32f45230f01c2fbe
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-12-01 21:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,53 +1,25 @@
-// Person class
 class Person {
-  constructor(name, age) {
-    this._name = name;
-    this._age = age;
-  }
-
-  // Getter for name
-  get name() {
-    return this._name;
-  }
-
-  // Getter for age
-  get age() {
-    return this._age;
-  }
-
-  // Setter for age
-  set age(newAge) {
-    this._age = newAge;
-  }
+	constructor(name , age) {
+		this.name = name;
+		this.age = age;
+	}
+	get name (){
+		return this.name;
+	}
+	get age (){
+		return this.age;
+	}
+	set age(value){
+		this.age = value;
+	}
 }
-
-// Student class (inherits from Person)
 class Student extends Person {
-  study() {
-    console.log(`${this.name} is studying`);
-  }
+	study() {
+		console.log(`${this.name} is studying`)
+	}
 }
-
-// Teacher class (inherits from Person)
 class Teacher extends Person {
   teach() {
     console.log(`${this.name} is teaching`);
   }
 }
-
-// Example Usage
-
-// Creating a Person instance
-const person = new Person("John", 25);
-console.log(person.name); // Output: John
-
-person.age = 30; // Using the setter
-console.log(person.age); // Output: 30
-
-// Creating a Student instance
-const student = new Student("Alice", 22);
-student.study(); // Output: Alice is studying
-
-// Creating a Teacher instance
-const teacher = new Teacher("Bob", 40);
-teacher.teach(); // Output: Bob is teaching
